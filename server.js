99c6f7b2635d5bd913adec536048189ef2289d85
Utkarsh Sangwan <utkarshsangwan7@gmail.com>
2021-05-07 19:36:20 +0530

    gdgfdgdfgd

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ const appp = new Clarifai.App({
 const db = knex({
 	client:'pg',
 	connection: {
-		connectionString:process.env.DATABASE_URL,
-		ssl:true
+		connectionString:process.env.DATABASE_URL
+		// ssl:true
 	}
 });
 
